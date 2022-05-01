b73ffc591db0ef9d19d1f00b504d18add241b355
Anna <agalucnko@gmail.com>
2022-05-01 23:02:36 +0300

    add mobile-menu

--- js/mobill-menu.js
+++ js/mobill-menu.js
@@ -1,7 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
-  ​
+    
     menuBtnRef.addEventListener("click", () => {
       const expanded =
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
